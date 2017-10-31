0d4b72224cf774bf45719b8ff4d591635439c487
Shunsuke KITADA <septemtrio.ager@gmail.com>
2017-11-01 01:42:19 +0900

    update home scripts

--- nicole_or_kiko/home/models.py
+++ nicole_or_kiko/home/models.py
@@ -1,3 +1,8 @@
+from django import forms
 from django.db import models
 
 # Create your models here.
+
+
+class InputSentence(forms.Form):
+    sentence = forms.CharField()
--- nicole_or_kiko/home/views.py
+++ nicole_or_kiko/home/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+import os
+import pickle
 
-# Create your views here.
+import MeCab
+import numpy as np
+
+from sklearn.externals import joblib
+
+from django.shortcuts import render
+from nicole_or_kiko.settings import BASE_DIR
+from home.models import InputSentence
+
+# load classifier
+clf = joblib.load(os.path.join(BASE_DIR, "classifier", "classifier.pkl"))
+
+
+def index(request):
+
+    sentence = InputSentence()
+
+    context = {
+        "sentence": sentence
+    }
+    return render(request, "home/index.html", context)
+
+
+def result(request):
+
+    sentence = request.POST["sentence"]
+
+    pred = predict(sentence)
+
+    context = {
+        "sentence": sentence,
+        "pred": pred
+    }
+
+    return render(request, "home/result.html", context)
+
+
+def predict(sentence):
+
+    mt = MeCab.Tagger("-Owakati -d /usr/lib/mecab/dic/mecab-ipadic-neologd")
+    X = np.array([mt.parse(sentence)])
+
+    return clf.predict(X)
